b16076e924cb6326367b31e91f728acaf8aba970
Misha Mykhalyuk <m.mikhalyuk@viseven.com>
2016-11-29 08:59:37 +0200

    update(index.js): add power method

--- test/test.js
+++ test/test.js
@@ -1,26 +1,37 @@
-var assert = require('chai').assert,
-	calculator = require('../index.js');
+const chai = require('chai')
+const calculator = require('../index.js')
+const expect = chai.expect
+const assert = chai.assert
 
-describe("add", function(){
-	it('should add two number', function(){
-		assert.equal(calculator.add(2, 3), 5);
-	})
-});
+describe('add', function () {
+  it('Should add two numbers', function () {
+    assert.equal(calculator.add(2, 3), 5)
+  })
+})
 
-describe("subtract", function(){
-	it('should subtract two number', function(){
-		assert.equal(calculator.subtract(10, 3), 7);
-	})
-});
+describe('subtract', function () {
+  it('Should subtract two numbers', function () {
+    assert.equal(calculator.subtract(10, 3), 7)
+  })
+})
 
-describe("multiplication", function(){
-	it('should multiplication two number', function(){
-		assert.equal(calculator.multiplication(2, 3), 6);
-	})
-});
+describe('multiplication', function () {
+  it('Should multiplication two numbers', function () {
+    assert.equal(calculator.multiplication(2, 3), 6)
+  })
+})
 
-describe("division", function(){
-	it('should division two number', function(){
-		assert.equal(calculator.division(6, 3), 2);
-	})
-});
+describe('division', function () {
+  it('Should division two numbers', function () {
+    assert.equal(calculator.division(6, 3), 2)
+  })
+})
+
+describe('power', function () {
+  it('Should power number', function () {
+    const powered = calculator.pow(7, 2)
+
+	expect(powered).to.be.a('number')
+	expect(powered).to.equal(49)
+  })
+})
